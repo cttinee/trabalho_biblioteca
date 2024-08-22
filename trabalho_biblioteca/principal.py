a739f5726419fbcee06c3f8b3826117edb914893
Evelyn Christine <evelyncasodre@gmail.com>
2024-08-22 16:55:10 -0300

    Update principal.py

--- trabalho_biblioteca/principal.py
+++ trabalho_biblioteca/principal.py
@@ -23,23 +23,23 @@ def main():
         opcao = input("Escolha uma opção: ")
 
         if opcao == '1':
-            nome = input("Digite o novo nome do usuário: ")
+            nome = input("Digite o novo nome do usuário: ").lower()
             adicionar_usuario(nome)
             print(f'Usuário "{nome}" adicionado com sucesso!')
         elif opcao == '2':
-            titulo = input("Digite o título do livro: ")
-            autor = input("Digite o autor do livro: ")
+            titulo = input("Digite o título do livro: ").lower()
+            autor = input("Digite o autor do livro: ").lower()
             adicionar_livro(titulo, autor)
             print(f'Livro "{titulo}" adicionado com sucesso!')
         elif opcao == '3':
             exibir_livros()
         elif opcao == '4':
-            nome_usuario = input("Digite o nome do usuário: ")
-            titulo_livro = input("Digite o título do livro: ")
+            nome_usuario = input("Digite o nome do usuário: ").lower()
+            titulo_livro = input("Digite o título do livro: ").lower()
             resultado = emprestar_livro(nome_usuario, titulo_livro)
             print(resultado)
         elif opcao == '5':
-            titulo_livro = input("Digite o título do livro: ")
+            titulo_livro = input("Digite o título do livro: ").lower()
             resultado = devolver_livro(titulo_livro)
             print(resultado)
         elif opcao == '6':
